d2c1f69fb7e583e0cd18fdadf3b5c9d6036dee02
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2024-09-10 10:54:45 -0300

    Add arm64 support for DCGM
    Download the `cuda-keyring_1.1-1_all.deb` package depending on the
    architecture of the system, check the sha256sum and install if
    valid. This package will configure the apt sources and it's the
    recommended way by the [documentation](https://docs.nvidia.com/datacenter/dcgm/latest/user-guide/getting-started.html#ubuntu-lts-and-debian)

---
+++ snap/local/configure_sources.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DISTRIBUTION="ubuntu2404"
+SYSTEM_ARCH=$(uname -m)
+# package that configures the apt source
+CUDA_PKG="cuda-keyring_1.1-1_all.deb"
+
+if [ "$SYSTEM_ARCH" = "aarch64" ]; then
+    ARCH="sbsa"
+    SHA256SUM="6ea7d2737648936820e85677177957a0f6521b840d98eb0bbae0a4f003fa7249"
+elif [ "$SYSTEM_ARCH" = "x86_64" ]; then
+    ARCH="x86_64"
+    SHA256SUM="d2a6b11c096396d868758b86dab1823b25e14d70333f1dfa74da5ddaf6a06dba"
+else
+    echo "Unsupported architecture: $SYSTEM_ARCH"
+    exit 1
+fi
+
+
+echo "Architecture is $SYSTEM_ARCH. Downloading cuda-keyring package..."
+wget "https://developer.download.nvidia.com/compute/cuda/repos/$DISTRIBUTION/$ARCH/$CUDA_PKG"
+
+# Run the checksum verification and install cuda-keyring if valid
+if echo "$SHA256SUM  $CUDA_PKG" | sha256sum --check --status; then
+    echo "Checksum for $CUDA_PKG is correct."
+    dpkg -i $CUDA_PKG
+    apt-get update
+else
+    echo "Checksum for $CUDA_PKG is incorrect."
+    exit 1
+fi
